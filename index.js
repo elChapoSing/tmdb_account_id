ef7f08e993c5eced55b0e90b204d12f54bf95b53
elChapoSing <github@pochat.eu>
2019-05-08 23:09:15 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+
+let getToken = () => {
+
+    // eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZDJmMjA2NTNmMWEwNTE4ZDk4Yzk5OTc2ODY1OTRhYSIsInN1YiI6IjVjY2VhNTlkYzNhMzY4MzZiMGUwMWFlYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CGYJa0MSdMT0lUHkeLqs5oqaxAF9-X5KHLbBm5Zn79w
+
+    let url = "https://api.themoviedb.org/4/auth/request_token";
+    let options = {
+        method: "POST",
+        headers: {
+            authorization: "Bearer " + $("#tmdb_read_access_token").val()
+        }
+    };
+    $.ajax(url, options).then((body) => {
+
+        let request_token = body.request_token;
+        $("#tmdb_request_token").val(request_token);
+        $("#tmdb_auth_url").val("https://www.themoviedb.org/auth/access?request_token=" + request_token)
+
+    }).catch((err) => {
+        $("#tmdb_auth_url").val(JSON.stringify(err));
+    });
+};
+
+let goAuth = () => {
+
+    window.open($("#tmdb_auth_url").val(),"_blank");
+
+};
+
+
+let getAuth = () => {
+    let url = "https://api.themoviedb.org/4/auth/access_token";
+    let options = {
+        method:"POST",
+        headers : {
+            authorization: "Bearer " + $("#tmdb_read_access_token").val()
+        },
+        data:{
+            request_token: $("#tmdb_request_token").val()
+        }
+    };
+    $.ajax(url,options).then((body) => {
+        $("#account_id").val(body.account_id);
+    }).catch((err)=> {
+        $("#account_id").val(err);
+    });
+};
+$(function() {
+
+    $("#btn_get_req_token")
+        .button()
+        .click(getToken);
+    $("#btn_start_auth")
+        .button()
+        .click(goAuth);
+    $("#btn_get_auth")
+        .button()
+        .click(getAuth);
+
+});
